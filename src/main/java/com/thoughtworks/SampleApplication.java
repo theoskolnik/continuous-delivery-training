eb913c17b3d99006fd8fb48a4b17e119a0ff561c
Chris Kozak <ckozak@gmail.com>
2015-08-13 23:27:02 -0500

    Adding static files

--- src/main/java/com/thoughtworks/SampleApplication.java
+++ src/main/java/com/thoughtworks/SampleApplication.java
@@ -1,11 +1,26 @@
 package com.thoughtworks;
 
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.*;
 
 public class SampleApplication {
     public static void main(String[] args) {
         port(Integer.valueOf(System.getenv("PORT")));
+        staticFileLocation("/public");
+
         get("/hello", (req, res) -> "Hello World");
+
+        get("/", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("message", "Hello World!");
+
+            return new ModelAndView(attributes, "index.ftl");
+        }, new FreeMarkerEngine());
     }
 }
 
