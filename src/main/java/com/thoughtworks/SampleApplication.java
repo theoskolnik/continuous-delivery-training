1c411a20e82dd8289089cfdeae1f74a793dc2f06
Chris Kozak <ckozak@gmail.com>
2015-08-13 22:39:30 -0500

    Changing port

--- src/main/java/com/thoughtworks/SampleApplication.java
+++ src/main/java/com/thoughtworks/SampleApplication.java
@@ -4,6 +4,7 @@ import static spark.Spark.*;
 
 public class SampleApplication {
     public static void main(String[] args) {
+        port(8080);
         get("/hello", (req, res) -> "Hello World");
     }
 }
